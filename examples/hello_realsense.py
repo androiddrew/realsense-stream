87af921d76e4aa7fbc35af1aaa56cc2b300c1d59
Drew Bednar <bednar.andrew@gmail.com>
2020-06-02 22:47:07 -0400

    Correcting the hello world example

--- examples/hello_realsense.py
+++ examples/hello_realsense.py
@@ -1,5 +1,5 @@
 import logging
-import realsense2 as rs
+import pyrealsense2 as rs
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -17,8 +17,8 @@ def main():
         # This is a blocking call poll_for_frames can be used for async
         frame_set = pipeline.wait_for_frames()
         depth_frame = frame_set.get_depth_frame()
-        width, height = depth_frame.get_width(), depth_frame.get_heigt()
-        dist_to_center = depth_frame.get_distance(width / 2, height / 2)
+        width, height = depth_frame.get_width(), depth_frame.get_height()
+        dist_to_center = depth_frame.get_distance(width // 2, height // 2)
         logger.info("The camera is facing an object {} meters away.".format(dist_to_center))
 
 
